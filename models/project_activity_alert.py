5d06f8006481be827abbbd60f51737ff06ca2cad
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2024-08-19 18:58:13 -0300

    cambios model

--- models/project_activity_alert.py
+++ models/project_activity_alert.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 import logging
 from odoo.exceptions import UserError
 
@@ -7,10 +7,9 @@ _logger = logging.getLogger(__name__)
 class ProjectActivityAlert(models.Model):
     _inherit = 'res.users'
 
-    @api.model
     def check_pending_activities(self):
+        user = self.env.user
         try:
-            user = self.env.user
             pending_activities = self.env['project.task'].search([
                 ('user_id', '=', user.id),
                 ('stage_id.fold', '=', False),
@@ -18,18 +17,17 @@ class ProjectActivityAlert(models.Model):
             ])
 
             if pending_activities:
-                # Mostrar notificación al usuario
                 return {
                     'type': 'ir.actions.client',
                     'tag': 'display_notification',
                     'params': {
-                        'title': 'Actividades Pendientes',
-                        'message': f'Tienes {len(pending_activities)} actividades pendientes en el proyecto.\n Por favor revisalas.',
+                        'title': _('Actividades Pendientes'),
+                        'message': _('Tienes %s actividades pendientes en el proyecto.\n Por favor revisalas.') % len(pending_activities),
                         'sticky': False,
                     }
                 }
         except Exception as e:
-            _logger.error("Error checking pending activities for user %s: %s", self.env.user.id, str(e))
+            _logger.error("Error checking pending activities for user %s: %s", user.id, str(e))
             raise UserError(_('Ha ocurrido un error al verificar las actividades pendientes: %s') % str(e))
 
     @api.model
@@ -37,9 +35,6 @@ class ProjectActivityAlert(models.Model):
         try:
             users = self.env['res.users'].search([])
             for user in users:
-                # Reconfiguramos el entorno para el usuario actual
-                self = user
-                self.check_pending_activities()
+                self.with_user(user).check_pending_activities()
         except Exception as e:
             _logger.error("Error during scheduled activity check: %s", str(e))
-            # Opcional: Puedes registrar un mensaje de error más general o específico
