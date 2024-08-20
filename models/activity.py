7376a4daaab4721f6f7d625771be15129ea4533a
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2024-08-19 22:46:14 -0300

    Prueba 3

--- models/activity.py
+++ models/activity.py
@@ -1,13 +1,16 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class Activity(models.Model):
     _inherit = 'mail.activity'
 
     @api.model
-    def get_pending_activities(self):
+    def check_pending_activities(self):
         user = self.env.user
         activities = self.search([
             ('user_id', '=', user.id),
             ('state', '!=', 'preparado')
         ])
-        return activities
+        if activities:
+            raise UserError(f"Tienes {len(activities)} actividades pendientes.")
+
