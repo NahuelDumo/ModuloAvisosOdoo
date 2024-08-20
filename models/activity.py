4a8097ebcab80c9949d21c7f6fa97151c4b452ab
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2024-08-19 22:52:31 -0300

    prueba 4

--- models/activity.py
+++ models/activity.py
@@ -1,16 +1,26 @@
 from odoo import models, fields, api
-from odoo.exceptions import UserError
+import logging
 
-class Activity(models.Model):
-    _inherit = 'mail.activity'
+_logger = logging.getLogger(__name__)
 
-    @api.model
-    def check_pending_activities(self):
-        user = self.env.user
-        activities = self.search([
-            ('user_id', '=', user.id),
-            ('state', '!=', 'preparado')
-        ])
-        if activities:
-            raise UserError(f"Tienes {len(activities)} actividades pendientes.")
+class ResUsers(models.Model):
+    _inherit = 'res.users'
 
+    pending_activities_message = fields.Char(
+        compute='_compute_pending_activities_message',
+        store=False
+    )
+
+    def _compute_pending_activities_message(self):
+        for user in self:
+            pending_activities = self.env['project.task'].search([
+                ('user_id', '=', user.id),
+                ('stage_id.fold', '=', False),
+                ('date_deadline', '<=', fields.Date.today()),
+            ])
+
+            if pending_activities:
+                user.pending_activities_message = f'Tienes {len(pending_activities)} actividades pendientes en el proyecto.\nPor favor revísalas.'
+                _logger.info(f"Usuario {user.name}: {user.pending_activities_message}")
+            else:
+                user.pending_activities_message = ''
