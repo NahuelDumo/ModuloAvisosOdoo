e8ce1ebe912249d9d9a89211897cf0d3422579a6
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2024-08-20 22:35:19 -0300

    Modificacion

--- models/activity.py
+++ models/activity.py
@@ -1,30 +1,35 @@
 from odoo import models, fields, api
 
 class ActivityReminder(models.Model):
-    _name = 'moduloavisosodoo.activity_reminder'  # ejemplo válido
-
+    _name = 'moduloavisosodoo.activity_reminder'
     _description = 'Activity Reminder'
 
     message = fields.Char(string='Reminder Message', compute='_compute_reminder_message')
 
-    @api.model
+    @api.depends('message')
+    def _compute_reminder_message(self):
+        for record in self:
+            record.message = record.get_reminder_message()
+
     def get_pending_activities(self):
         # Obtener las tareas pendientes del usuario actual
         tasks = self.env['project.task'].search([('user_id', '=', self.env.uid), ('stage_id', '=', False)])
         return len(tasks)
 
-    @api.model
     def get_reminder_message(self):
         pending_activities = self.get_pending_activities()
         if pending_activities > 0:
             return f'Tienes {pending_activities} actividades pendientes. Por favor, revise las tareas asignadas.'
         else:
-            return ''
+            return 'No tienes actividades pendientes.'
 
-    @api.model
     def get_reminder_color(self):
         pending_activities = self.get_pending_activities()
         if pending_activities > 0:
             return 'danger'  # Rojo
         else:
             return 'success'  # Verde
+
+    def action_show_reminder(self):
+        # Actualizar el mensaje de recordatorio
+        self.message = self.get_reminder_message()
